a705cf6420833386c8d34174f0fe3dd7fba9006b
Jakub Podlesak <jakub.podlesak@oracle.com>
2019-06-05 12:27:14 +0200

    fixes for test result graph rendering

--- src/main/java/org/tap4j/plugin/TapResult.java
+++ src/main/java/org/tap4j/plugin/TapResult.java
@@ -2,17 +2,17 @@
  * The MIT License
  *
  * Copyright (c) 2011 Bruno P. Kinoshita
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -189,6 +189,9 @@ public class TapResult implements ModelObject, Serializable {
 
         for (TestSetMap testSet : testSets) {
             TestSet realTestSet = testSet.getTestSet();
+            if (realTestSet == null) {
+                continue;
+            }
             List<TestResult> testResults = realTestSet.getTestResults();
 
             total += testResults.size();
@@ -327,7 +330,7 @@ public class TapResult implements ModelObject, Serializable {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see hudson.model.ModelObject#getDisplayName()
      */
     public String getDisplayName() {
